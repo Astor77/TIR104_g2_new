d173d66da156c07824f5a946fe1d72ef54721543
Astor <astor.li0806@gmail.com>
2025-02-19 21:34:04 +0800

    新增flow0, 外加flow2更新

--- src/tasks/Fetching_Task/fetch_crawl_data_module.py
+++ src/tasks/Fetching_Task/fetch_crawl_data_module.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pandas as pd
 import requests
 import time
@@ -57,10 +56,4 @@ def crawl_movie_info(movie_id, url, api_token, headers):
 
 
 # get_numbers_movie_box(全球資料)
-=======
-# fetch_crawl_data_task.py -> 整合用爬蟲獲取的函式
-
-
-# get_numbers_movie_box(全球資料)
->>>>>>> main
 # 來源為the numbers
