a17132c38757574b29e9a50e9d9d2f4978ebdeae
Rain <tsairen17@gmail.com>
2025-02-26 11:47:43 +0800

    更新資料

--- A_origin_pyfile/Rain/iso3166_countries/tmdb_iso3166_country.py
+++ A_origin_pyfile/Rain/iso3166_countries/tmdb_iso3166_country.py
@@ -3,7 +3,7 @@ import json
 import pandas as pd
 
 # 讀取上傳的 JSON 檔案
-file_path = "iso3166_country.json"
+file_path ="iso3166_country.json"
 with open(file_path, "r", encoding="utf-8") as file:
     data = json.load(file)  # 這行需要縮排
 
@@ -23,3 +23,5 @@ df = pd.DataFrame(countries_data)
 # 保存為 CSV 檔案
 csv_file_path = "tmdb_release_country.csv"
 df.to_csv(csv_file_path, index=False, encoding="utf-8-sig")
+
+
