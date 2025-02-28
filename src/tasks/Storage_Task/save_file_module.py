4a9e4e61b82d992536ed4c21ef3f7907c2d0cac7
Astor <astor.li0806@gmail.com>
2025-02-28 16:02:01 +0800

    環境更新, flow2更新, save_file_module更新csv資料型態處理，以利BigQuery上傳流程

--- src/tasks/Storage_Task/save_file_module.py
+++ src/tasks/Storage_Task/save_file_module.py
@@ -10,11 +10,11 @@ from utils.write_log_task import write_save_log
 # 儲存 DataFrame 為 CSV 檔案
 def save_as_csv(dataframe: pd.DataFrame, dir_path: str | Path, file_name: str) -> None:
     """
-    將 DataFrame 儲存為 CSV 檔案
+    將 DataFrame 儲存為 CSV 檔案，並且保留原始dataframe的資料型態
     Args:
         dataframe (pd.DataFrame): 要儲存的 DataFrame
-        file_name (str): 儲存的檔案名稱字串
         dir_path (str): 儲存的資料夾完整路徑
+        file_name (str): 儲存的檔案名稱字串
     """
     try:
         dir_path = Path(dir_path)  # 確保轉成 Path 物件
@@ -36,21 +36,25 @@ def save_as_csv(dataframe: pd.DataFrame, dir_path: str | Path, file_name: str) -
 
         write_save_log("save_as_csv", file_name, "success", "檔案儲存成功", csv_file_path)
         print(f"{file_name} 儲存成功, 存放路徑: {csv_file_path}")
+        save_result = f"{file_name} 儲存成功"
+        return save_result
 
     except Exception as err:
         write_save_log("save_as_csv", file_name, "fail", f"{err}", csv_file_path)
         print(f"{file_name} 儲存失敗。error: {err}")
+        save_result = f"{file_name} 儲存失敗, error: {err}"
+        return save_result
 
 # save_as_json()
 # 將 list 儲存為 JSON 檔案
 # 留意此處的 data 引數需要用list type
-def save_as_json(data: list, file_name: str, dir_path: str | Path) -> None:
+def save_as_json(data: list | dict, dir_path: str | Path, file_name: str) -> None:
     """
-    將 list 儲存為 JSON 檔案
+    將 list 或 dict 儲存為 JSON 檔案
     Args:
-        data (list): 要儲存的 DataFrame
-        file_name (str): 儲存的檔案名稱字串
+        data (list| dict): 要儲存的原始資料
         dir_path (str): 儲存的資料夾完整路徑
+        file_name (str): 儲存的檔案名稱字串
     """
     try:
         dir_path = Path(dir_path)  # 確保轉成 Path 物件
@@ -64,7 +68,11 @@ def save_as_json(data: list, file_name: str, dir_path: str | Path) -> None:
 
             write_save_log("save_as_json", file_name, "success", "檔案儲存成功", json_file_path)
             print(f"{file_name} 儲存成功, 存放路徑: {json_file_path}")
+            save_result = f"{file_name} 儲存成功"
+            return save_result
 
     except Exception as err:
         write_save_log("save_as_json", file_name, "fail", f"{err}", json_file_path)
         print(f"{file_name} 儲存失敗。error: {err}")
+        save_result = f"{file_name} 儲存失敗, error: {err}"
+        return save_result
